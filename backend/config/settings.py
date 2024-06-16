8b3b6b1a4079cd2fceb6dfb4e298df2382519813
NECROshizo <necroshizo@gmail.com>
2024-06-16 09:59:26 +0300

    add custom User

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -5,9 +5,9 @@ from pathlib import Path
 from django.core.management.utils import get_random_secret_key
 from dotenv import load_dotenv
 
-load_dotenv()
-
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv(os.path.join(BASE_DIR, '..', 'infra', '.env'))
 
 SECRET_KEY = os.getenv('PRODUCT_KEY', default=get_random_secret_key())
 
@@ -15,7 +15,7 @@ DEBUG = os.getenv('DEBUG_MODE', 'True') == 'True'
 
 ALLOWED_HOSTS = ['*']
 
-# AUTH_USER_MODEL = 'users.User'
+AUTH_USER_MODEL = 'user.User'
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -89,7 +89,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-LANGUAGE_CODE = 'ru'
+LANGUAGE_CODE = 'ru-RU'
 
 TIME_ZONE = 'Europe/Moscow'
 
@@ -99,11 +99,15 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
 MEDIA_URL = '/media/'
+STATIC_URL = '/static/'
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+if DEBUG is True:
+    STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
@@ -112,7 +116,6 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
 }
-
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
--- backend/user/models.py
+++ backend/user/models.py
@@ -1,1 +1,20 @@
-# Create your models here.
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
+
+
+class User(AbstractUser):
+    class Meta:
+        verbose_name = _('Пользователь')
+        verbose_name_plural = _('Пользователи')
+        db_table_comment = _('Пользователи')
+
+    @property
+    def is_admin(self):
+        return self.is_staff or self.is_superuser
+
+    @property
+    def is_user(self):
+        return not self.is_admin
+
+    def __str__(self) -> str:
+        return self.username
